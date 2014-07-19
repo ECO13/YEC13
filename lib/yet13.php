58f0baf71ae6567950a98377ff467ffe4035746e
ECO13 <ECO13@users.noreply.github.com>
2014-07-19 02:18:03 +0200

    Update yet13.php

--- lib/yet13.php
+++ lib/yet13.php
@@ -1,12 +1,12 @@
 <?php
 /*
-Your Embed Code by ECO13 (YEC:13)							a fork off Shortcodes 1.8.X
-	CMS:			Elgg 1.8.X									Elgg 1.8.X
-	author:			Axel Vanderhaeghen							Mohammed Aqeel
-	organisation:	ECO13										Team Webgalli
-	url:			http://r-evolutie.net						http://webgalli.com
-	licence:		GNU General Public License, version 3		GNU General Public License, version 2
-	copyright: 		2014 © ECO13								2011-2015 © Team Webgalli
+Your Embed Code by ECO13 (YEC:13)					a fork off Shortcodes 1.8.X
+CMS:			Elgg 1.8.X					Elgg 1.8.X
+author:			Axel Vanderhaeghen				Mohammed Aqeel
+organisation:		ECO13						Team Webgalli
+url:			http://r-evolutie.net				http://webgalli.com
+licence:		GNU General Public License, version 3		GNU General Public License, version 2
+copyright: 		2014 © ECO13					2011-2015 © Team Webgalli
 */
 
  
